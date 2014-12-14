8b022fa6fe114a441f291e18f8e12649f1cbdaf1
iluvredwall <bobomb.asplode@gmail.com>
2014-12-13 21:50:22 -0800

    Finish renaming Event to Post

--- Extensions.AuditEventBus.cs
+++ Extensions.AuditEventBus.cs
@@ -7,27 +7,27 @@ using System.Threading.Tasks;
 namespace Dargon.Audits.Api {
    public static class Extensions {
       public static void Debug(this AuditEventBus bus, string eventKey, string message, string data) {
-         bus.Event(AuditEventType.Debug, eventKey, message, data);
+         bus.Post(AuditEventType.Debug, eventKey, message, data);
       }
 
       public static void Verbose(this AuditEventBus bus, string eventKey, string message, string data = null) {
-         bus.Event(AuditEventType.Verbose, eventKey, message, data);
+         bus.Post(AuditEventType.Verbose, eventKey, message, data);
       }
 
       public static void Notice(this AuditEventBus bus, string eventKey, string message, string data = null) {
-         bus.Event(AuditEventType.Notice, eventKey, message, data);
+         bus.Post(AuditEventType.Notice, eventKey, message, data);
       }
 
       public static void Warning(this AuditEventBus bus, string eventKey, string message, string data = null) {
-         bus.Event(AuditEventType.Warning, eventKey, message, data);
+         bus.Post(AuditEventType.Warning, eventKey, message, data);
       }
 
       public static void Error(this AuditEventBus bus, string eventKey, string message, string data = null) {
-         bus.Event(AuditEventType.Error, eventKey, message, data);
+         bus.Post(AuditEventType.Error, eventKey, message, data);
       }
 
       public static void Fatal(this AuditEventBus bus, string eventKey, string message, string data = null) {
-         bus.Event(AuditEventType.Fatal, eventKey, message, data);
+         bus.Post(AuditEventType.Fatal, eventKey, message, data);
       }
    }
 }
