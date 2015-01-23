0e1aaa47d6d7c63daf835be3b21ccc7edf6a1774
iluvredwall <bobomb.asplode@gmail.com>
2015-01-22 21:16:49 -0800

    Add Database interface

---
+++ Database.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Dargon.Audits {
+   public interface Database {
+      void Add(AuditEvent auditEvent);
+      IList<AuditEvent> Get();
+      IList<AuditEvent> Get(int index);
+   }
+}
